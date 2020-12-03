743148079388a84e8312696d358775ca82cdb022
Hanwei Wang (Beyondsoft) <v-hanweiwang@microsoft.com>
2020-12-03 18:30:32 +0800

    news more pages

---
+++ run.sh
@@ -0,0 +1,5 @@
+#python scrach.py
+
+python create_db.py
+
+python ./web/website.py
--- scratch.py
+++ scratch.py
@@ -26,84 +26,97 @@ def get_soup(url, encoding="utf-8"):
         return None
 
 
-def get_news_item(url):
+def get_news_item(category, root_url):
     news_list = []
-    n_pages = 2 # scracth n pages
-    print(f'\nGet {n_pages} pages of news item in {url} ...')
+    n_pages = 2 # scracth n pages for each category
 
-    for page in range(1,n_pages+1):
-        page_url = url.format(str(page))
-        soup = get_soup(page_url)
-        if soup is None:
-            continue
-        news_ul = soup.find('ul', {'class': 'txt-list-a fz-14'})
-        news_li = news_ul.find_all('li')
-        for news in news_li:
-            try:
-                # check if news is empty
-                if len(news.text) == 0:
-                    continue
+    for c in category:
+        url = root_url + c
+        print(f'\nGet {n_pages} pages of news item in {url} ...')
 
-                news_time = news.span.text
-                title = news.a.text.strip()
-                news_url = news.a['href']
-                news_info = {
-                    "time":news_time,
-                    "title": title,
-                    "url": news_url
-                }
-                
-                news_list.append(news_info)
-            except Exception as e:
-                print("get_news_item: %s : %s" % (type(e), url))
+        for page in tqdm(range(1,n_pages+1)):
+            page_url = url + str(page) + '.shtml'
+            soup = get_soup(page_url)
+            if soup is None:
                 continue
+            news_ul = soup.find('ul', {'class': 'txt-list-a fz-14'})
+            news_li = news_ul.find_all('li')
+            for news in news_li:
+                try:
+                    # check if news is empty
+                    if len(news.text) == 0:
+                        continue
+
+                    news_time = news.span.text
+                    title = news.a.text.strip()
+                    news_url = news.a['href']
+                    news_info = {
+                        "time":news_time,
+                        "title": title,
+                        "url": news_url
+                    }
+                    
+                    news_list.append(news_info)
+                except Exception as e:
+                    print("get_news_item: %s : %s" % (type(e), url))
+                    continue
     print('\nSuccessful get news list!\n')
     return news_list
 
 
 def get_news_info(news_list, doc_file_path, doc_encoding="utf-8"):
-    print('\nGet news info in news list ...\n')
-
+    print('Get news info in news list ...\n')
     index = 1
 
-    for news in news_list:
+    for news in tqdm(news_list):
         new_url = news.get('url')
-        soup = get_soup(new_url)
+        news_page = 1
+        doc_text = ""
+
+        # news may have more than one pages
+        while True:
+            if news_page == 1:
+                soup = get_soup(new_url)
+            else:
+                soup = get_soup(new_url[:-6]+'_'+str(news_page)+'.shtml')
+            
+            try:
+                doc = soup.find("div", {"class": "articleText"})   #find in web page
+                if doc is None:
+                    break
+                doc_p = doc.find_all('p')
+                for t in doc_p:
+                    doc_text += t.text.strip()
+            except Exception as e:
+                print("get_news_info: %s : %s" % (type(e), new_url))
+            
+            news_page += 1
 
         # check if news url is empty
-        if soup is None:
+        if len(doc_text) == 0:
             continue
-        
-        try:
-            doc = soup.find("div", {"class": "articleText"})   #find in web page
-            doc_p = doc.find_all('p')
-            doc_text = ""
-            for t in doc_p:
-                doc_text += t.text.strip()
-            
-            # clean doc_text
 
-            # write xml doc
-            new_ET = ET.Element('news')
-            ET.SubElement(new_ET, 'id').text = "%d"%(index)
-            ET.SubElement(new_ET, 'url').text = news.get('url')
-            ET.SubElement(new_ET, 'title').text = news.get('title')
-            ET.SubElement(new_ET, 'datetime').text = news.get('time')
-            ET.SubElement(new_ET, 'body').text = doc_text
-            tree = ET.ElementTree(new_ET)
-            tree.write(file_or_filename = doc_file_path+str(index)+'.xml', encoding = doc_encoding, xml_declaration = True)
+        # clean doc_text
 
-            print(f"Successful written news {index}!")
-            index += 1
-        except Exception as e:
-            print("get_news_info: %s : %s" % (type(e), new_url))
+        # write xml doc
+        new_ET = ET.Element('news')
+        ET.SubElement(new_ET, 'id').text = "%d"%(index)
+        ET.SubElement(new_ET, 'url').text = news.get('url')
+        ET.SubElement(new_ET, 'title').text = news.get('title')
+        ET.SubElement(new_ET, 'datetime').text = news.get('time')
+        ET.SubElement(new_ET, 'body').text = doc_text
+        tree = ET.ElementTree(new_ET)
+        tree.write(file_or_filename = doc_file_path+str(index)+'.xml', encoding = doc_encoding, xml_declaration = True)
+
+        index += 1
 
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read("./config.ini", encoding="utf-8")
-    url = "http://www.cankaoxiaoxi.com/china/szyw/{}.shtml"
-    news_list = get_news_item(url)
+    category = ['china/szyw/']
+    root_url = "http://www.cankaoxiaoxi.com/"
+    news_list = get_news_item(category, root_url)
     get_news_info(news_list, config['DEFAULT']['doc_file_path'])
 
 
